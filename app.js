3c32eb1e3bb18bec664ffa012cfbdda5c70ebf18
yodfhafx <yodfha2014@gmail.com>
2019-01-07 04:23:54 +0700

    create models and connect mongoose

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const app = express();
+const mongoose = require('mongoose');
 
+const app = express();
 const port = 3000;
+
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/videojs-dev', {
+  useMongoClient: true
+})
+.then(() => console.log('MongoDB connected...'))
+.catch(err => console.log(err));
+
+// Load Video Model
+require('./models/Video');
+const Video = mongoose.model('videos');
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
