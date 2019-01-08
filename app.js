b1a035c19627c0337e28b68ebba961114635ca6f
yodfhafx <yodfha2014@gmail.com>
2019-01-08 12:22:58 +0700

    create login and register form

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override')
 const flash = require('connect-flash');
@@ -30,6 +31,9 @@ app.set('view engine', 'handlebars');
 // Body parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Method override middleware
 app.use(methodOverride('_method'));
